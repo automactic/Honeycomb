feac0014409f36a49888d63b56fdcf1daaa322c5
Chris Li <chris@kiwix.org>
2023-10-10 09:02:40 -0400

    albums

--- View/AlbumsView.swift
+++ View/AlbumsView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct AlbumsView: View {
+    @Environment(\.horizontalSizeClass) private var horizontalSizeClass
     @State private var viewModel: AlbumsViewModel
     
     let tab: Tab
@@ -22,7 +23,7 @@ struct AlbumsView: View {
             if viewModel.albums.isEmpty, viewModel.allPagesLoaded {
                 ContentUnavailableView("No Folders", systemImage: tab.icon)
             } else {
-                AlbumsGridView()
+                AlbumGridView()
             }
         }
         .autocorrectionDisabled()
@@ -38,33 +39,49 @@ struct AlbumsView: View {
                 LoadingView().padding()
             }
         }
+        .task {
+            guard viewModel.albums.isEmpty else { return }
+            await viewModel.reload()
+        }
+        .onChange(of: viewModel.searchText) {
+            Task {
+                await viewModel.reload()
+            }
+        }
     }
 }
 
-struct AlbumsGridView: View {
-    @Environment(\.isSearching) private var isSearching
+struct AlbumGridView: View {
+    @Environment(\.horizontalSizeClass) private var horizontalSizeClass
     @Environment(AlbumsViewModel.self) private var viewModel
-    @State private var taskID: UUID?
     
     var body: some View {
-        ScrollView {
-            LazyVGrid(columns: [GridItem(.adaptive(minimum: 100, maximum: 250), spacing: 10)], spacing: 10) {
-                ForEach(viewModel.albums) { album in
-                    NavigationLink(value: album) {
-                        AlbumView(album: album)
-                    }.buttonStyle(.plain)
+        GeometryReader { geometry in
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: spacing) {
+                    ForEach(viewModel.albums) { album in
+                        NavigationLink(value: album) {
+                            AlbumView(album: album)
+                        }.buttonStyle(.plain)
+                    }
                 }
-            }
+            }.contentMargins(.horizontal, geometry.size.width > 400 ? 20 : 16, for: .scrollContent)
         }
-        .contentMargins(.horizontal, 16, for: .scrollContent)
-        .refreshable {
-            Task { await viewModel.reload() }
+    }
+    
+    private var columns: [GridItem] {
+        switch horizontalSizeClass {
+        case .regular:
+            [GridItem(.adaptive(minimum: 175, maximum: 225), spacing: spacing)]
+        case .compact:
+            [GridItem(.adaptive(minimum: 100, maximum: 150), spacing: spacing)]
+        default:
+            []
         }
-        .task(id: viewModel.searchText) {
-            print(isSearching)
-            guard isSearching || viewModel.albums.isEmpty else { return }
-            await viewModel.reload()
-        }
+    }
+    
+    private var spacing: CGFloat {
+        horizontalSizeClass == .regular ? 15 : 10
     }
 }
 
@@ -82,7 +99,7 @@ struct AlbumView: View {
             }.aspectRatio(1, contentMode: .fill)
             Text(album.title).font(.caption).lineLimit(1).padding(4)
         }
-        .background(Color(uiColor: .systemBackground))
+        .background(Color(uiColor: .secondarySystemGroupedBackground))
         .clipShape(RoundedRectangle(cornerRadius: 6, style: .continuous))
     }
 }
