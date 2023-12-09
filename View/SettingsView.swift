fd107c0478567f73700643404b83355ee1658387
Chris Li <chris@kiwix.org>
2023-12-09 16:23:39 -0500

    settings

--- View/SettingsView.swift
+++ View/SettingsView.swift
@@ -5,30 +5,64 @@
 //  Created by Chris Li on 10/1/23.
 //
 
+import SwiftData
 import SwiftUI
 
 struct SettingsView: View {
-    @AppStorage(StorageKeys.serverURL) private var serverURL = ""
-    @AppStorage(StorageKeys.sessionID) private var sessionID: String?
-    @AppStorage(StorageKeys.previewToken) private var previewToken: String?
     @Environment(\.modelContext) private var modelContext
+    @Query private var servers: [Server]
+    @State private var isAddingNewServer = false
     
     var body: some View {
-        Form {
-            Section {
-                AttributeRow(title: "Server", detail: serverURL)
-            }
-            Section {
-                Button("Sign Out", role: .destructive) {
-                    sessionID = nil
-                    previewToken = nil
-                    modelContext.container.deleteAllData()
+        List {
+            Section("Servers") {
+                ForEach(servers) { server in
+                    NavigationLink(value: server) {
+                        Text(server.description)
+                    }
+                }
+                Button("Add New Server") {
+                    isAddingNewServer = true
                 }
             }
-        }.navigationTitle("Settings")
+        }
+        .navigationTitle("Settings")
+        .navigationDestination(for: Server.self) { server in
+            Form {
+                Section {
+                    Text("Server").badge(server.url.absoluteString)
+                }
+                Section {
+                    Button("Sign Out", role: .destructive) {
+                        modelContext.delete(server)
+                    }
+                }
+            }
+            .navigationTitle(server.description)
+            .navigationBarTitleDisplayMode(.inline)
+        }
+        .sheet(isPresented: $isAddingNewServer) {
+            NavigationStack {
+                AddServerView().toolbar {
+                    ToolbarItem(placement: .cancellationAction) {
+                        Button("Cancel") {
+                            isAddingNewServer = false
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    SettingsView()
+    let config = ModelConfiguration(isStoredInMemoryOnly: true)
+    do {
+        let container = try ModelContainer(for: Server.self, configurations: config)
+        return NavigationStack {
+            SettingsView()
+        }.modelContainer(container)
+    } catch {
+        fatalError("Could not create ModelContainer: \(error)")
+    }
 }
