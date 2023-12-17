71b76db80aa210a2e8bff65801974542ba4944e3
Chris Li <chris@kiwix.org>
2023-12-17 09:33:06 -0500

    refactor

--- Widget/ItemCount.swift
+++ Widget/ItemCount.swift
@@ -24,6 +24,104 @@ enum CountableItem: String, AppEnum {
     ]
 }
 
+// MARK: - Model
+
+struct CounterConfig: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource = "Customize Widget"
+    
+    @Parameter(title: "Counter", default: .photos)
+    var item: CountableItem
+}
+
+struct CounterTimelineProvider: AppIntentTimelineProvider {
+    static let placeholderItemCounts = ServerConfig.Count(
+        all: 14769, photos: 11308, videos: 3453, archived: 12, favorites: 16, folders: 66
+    )
+    
+    func placeholder(in context: Context) -> CounterEntry {
+        CounterEntry(date: Date(), item: .photos, itemCounts: CounterTimelineProvider.placeholderItemCounts)
+    }
+    
+    func snapshot(for configuration: CounterConfig, in context: Context) async -> CounterEntry {
+        let fetchDescriptor = FetchDescriptor<Server>()
+//            fetchDescriptor.predicate = #Predicate<Server> { configuration.server.id == $0.id }
+        do {
+            let container = try ModelContainer(for: Server.self)
+            guard let server = try ModelContext(container).fetch(fetchDescriptor).first else {
+                return CounterEntry(date: Date(), item: configuration.item, itemCounts: nil)
+            }
+            let serverConfig = try await ServerConfig.get(server: server)
+            return CounterEntry(date: Date(), item: configuration.item, itemCounts: serverConfig.count)
+        } catch {
+            print(error)
+            return CounterEntry(date: Date(), item: configuration.item, itemCounts: nil)
+        }
+    }
+    
+    func timeline(for configuration: CounterConfig, in context: Context) async -> Timeline<CounterEntry> {
+        let entries = [await snapshot(for: configuration, in: context)]
+        guard let oneHourLater = Calendar.current.date(byAdding: .hour, value: 1, to: Date()) else {
+            return Timeline(entries: entries, policy: .never)
+        }
+        return Timeline(entries: entries, policy: .after(oneHourLater))
+    }
+}
+
+struct CounterEntry: TimelineEntry {
+    let date: Date
+    let item: CountableItem
+    let itemCounts: ServerConfig.Count?
+}
+
+// MARK: - View
+
+struct CountableItemIcon: View {
+    let item: CountableItem
+    
+    var body: some View {
+        switch item {
+        case .all:
+            Image(systemName: "infinity.circle").foregroundStyle(Color.purple)
+        case .photos:
+            Image(systemName: "photo.circle").foregroundStyle(Color.blue)
+        case .videos:
+            Image(systemName: "film.circle").foregroundStyle(Color.green)
+        case .archived:
+            Image(systemName: "archivebox.circle").foregroundStyle(Color.red)
+        case .favorites:
+            Image(systemName: "star.circle").symbolRenderingMode(.multicolor)
+        case .folders:
+            Image(systemName: "folder.circle").foregroundStyle(Color.orange)
+        }
+    }
+}
+
+struct CounterValue: View {
+    let item: CountableItem
+    let itemCounts: ServerConfig.Count
+    
+    var body: some View {
+        Text(count.formatted())
+    }
+    
+    private var count: Int {
+        switch item {
+        case .all:
+            return itemCounts.all
+        case .photos:
+            return itemCounts.photos
+        case .videos:
+            return itemCounts.videos
+        case .archived:
+            return itemCounts.archived
+        case .favorites:
+            return itemCounts.favorites
+        case .folders:
+            return itemCounts.folders
+        }
+    }
+}
+
 struct SingleCounterWidget: Widget {
     var body: some WidgetConfiguration {
         AppIntentConfiguration(
@@ -33,20 +131,7 @@ struct SingleCounterWidget: Widget {
         ) { entry in
             VStack(alignment: .trailing) {
                 HStack(alignment: .top) {
-                    switch entry.item {
-                    case .all:
-                        Image(systemName: "infinity.circle").imageScale(.large).foregroundStyle(Color.purple)
-                    case .photos:
-                        Image(systemName: "photo.circle").imageScale(.large).foregroundStyle(Color.blue)
-                    case .videos:
-                        Image(systemName: "film.circle").imageScale(.large).foregroundStyle(Color.green)
-                    case .archived:
-                        Image(systemName: "archivebox.circle").imageScale(.large).foregroundStyle(Color.red)
-                    case .favorites:
-                        Image(systemName: "star.circle").imageScale(.large).symbolRenderingMode(.multicolor)
-                    case .folders:
-                        Image(systemName: "folder.circle").imageScale(.large).foregroundStyle(Color.orange)
-                    }
+                    CountableItemIcon(item: entry.item).imageScale(.large)
                     Spacer()
                     VStack(alignment: .trailing) {
                         Text(CountableItem.caseDisplayRepresentations[entry.item]?.title ?? "").font(.headline)
@@ -67,7 +152,7 @@ struct SingleCounterWidget: Widget {
     struct Configuration: WidgetConfigurationIntent {
         static var title: LocalizedStringResource = "Customize Widget"
         
-        @Parameter(title: "Item", default: .photos)
+        @Parameter(title: "Counter", default: .photos)
         var item: CountableItem
     }
     
