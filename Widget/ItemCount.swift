9a9e5d30ca06dff60fb7963bc9369451bfd52bf3
Chris Li <chris@kiwix.org>
2023-12-17 09:48:29 -0500

    refactor

--- Widget/ItemCount.swift
+++ Widget/ItemCount.swift
@@ -73,7 +73,7 @@ struct CounterEntry: TimelineEntry {
     let itemCounts: ServerConfig.Count?
 }
 
-// MARK: - View
+// MARK: - Views
 
 struct CountableItemIcon: View {
     let item: CountableItem
@@ -122,12 +122,14 @@ struct CounterValue: View {
     }
 }
 
+// MARK: - Widgets
+
 struct SingleCounterWidget: Widget {
     var body: some WidgetConfiguration {
         AppIntentConfiguration(
             kind: "dev.chrisli.honeycomb.single-counter",
-            intent: Configuration.self,
-            provider: TimelineProvider()
+            intent: CounterConfig.self,
+            provider: CounterTimelineProvider()
         ) { entry in
             VStack(alignment: .trailing) {
                 HStack(alignment: .top) {
@@ -139,85 +141,25 @@ struct SingleCounterWidget: Widget {
                     }
                 }
                 Spacer()
-                Text(entry.count.formatted()).font(.system(.title, design: .rounded)).fontWeight(.semibold)
+                if let itemCounts = entry.itemCounts {
+                    CounterValue(item: entry.item, itemCounts: itemCounts)
+                        .font(.system(.title, design: .rounded)).fontWeight(.semibold)
+                }
             }.containerBackground(.fill.tertiary, for: .widget)
         }
         .configurationDisplayName("Counter")
         .description("A single counter of items in your PhotoPrism instance, such as photos, videos or favorites.")
         .supportedFamilies([.systemSmall])
     }
-    
-    // MARK: Config
-    
-    struct Configuration: WidgetConfigurationIntent {
-        static var title: LocalizedStringResource = "Customize Widget"
-        
-        @Parameter(title: "Counter", default: .photos)
-        var item: CountableItem
-    }
-    
-    // MARK: Timeline
-    
-    struct TimelineProvider: AppIntentTimelineProvider {
-        func placeholder(in context: Context) -> Entry {
-            Entry(date: Date(), item: .photos, count: 1024)
-        }
-        
-        func snapshot(for configuration: Configuration, in context: Context) async -> Entry {
-            let fetchDescriptor = FetchDescriptor<Server>()
-//            fetchDescriptor.predicate = #Predicate<Server> { configuration.server.id == $0.id }
-            do {
-                let container = try ModelContainer(for: Server.self)
-                guard let server = try ModelContext(container).fetch(fetchDescriptor).first else {
-                    return Entry(date: Date(), item: configuration.item, count: -2)
-                }
-                let serverConfig = try await ServerConfig.get(server: server)
-                let count: Int = {
-                    switch configuration.item {
-                    case .all:
-                        return serverConfig.count.all
-                    case .photos:
-                        return serverConfig.count.photos
-                    case .videos:
-                        return serverConfig.count.videos
-                    case .archived:
-                        return serverConfig.count.archived
-                    case .favorites:
-                        return serverConfig.count.favorites
-                    case .folders:
-                        return serverConfig.count.folders
-                    }
-                }()
-                return Entry(date: Date(), item: configuration.item, count: count)
-            } catch {
-                print(error)
-                return Entry(date: Date(), item: configuration.item, count: -1)
-            }
-        }
-        
-        func timeline(for configuration: Configuration, in context: Context) async -> Timeline<Entry> {
-            let entries = [await snapshot(for: configuration, in: context)]
-            guard let oneHourLater = Calendar.current.date(byAdding: .hour, value: 1, to: Date()) else {
-                return Timeline(entries: entries, policy: .never)
-            }
-            return Timeline(entries: entries, policy: .after(oneHourLater))
-        }
-    }
-
-    struct Entry: TimelineEntry {
-        let date: Date
-        let item: CountableItem
-        let count: Int
-    }
 }
 
 #Preview(as: .systemSmall) {
     SingleCounterWidget()
 } timeline: {
-    SingleCounterWidget.Entry(date: Date(), item: .all, count: 14623)
-    SingleCounterWidget.Entry(date: Date(), item: .photos, count: 10468)
-    SingleCounterWidget.Entry(date: Date(), item: .videos, count: 37)
-    SingleCounterWidget.Entry(date: Date(), item: .archived, count: 12)
-    SingleCounterWidget.Entry(date: Date(), item: .favorites, count: 16)
-    SingleCounterWidget.Entry(date: Date(), item: .folders, count: 66)
+    CounterEntry(date: Date(), item: .all, itemCounts: CounterTimelineProvider.placeholderItemCounts)
+    CounterEntry(date: Date(), item: .photos, itemCounts: CounterTimelineProvider.placeholderItemCounts)
+    CounterEntry(date: Date(), item: .videos, itemCounts: CounterTimelineProvider.placeholderItemCounts)
+    CounterEntry(date: Date(), item: .archived, itemCounts: CounterTimelineProvider.placeholderItemCounts)
+    CounterEntry(date: Date(), item: .favorites, itemCounts: CounterTimelineProvider.placeholderItemCounts)
+    CounterEntry(date: Date(), item: .folders, itemCounts: CounterTimelineProvider.placeholderItemCounts)
 }
